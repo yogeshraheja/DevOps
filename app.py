80421f62c79471f0e0592e0c53095c9eaa7ecdeb
adeepthinarayan <deepthi.narayan@thinknyx.com>
2025-09-15 16:16:44 +0530

    First commit for DevOps course

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Hello and Welcome to Thinknyx DevOps Associate Program!"
+
+@app.route("/services")
+def services():
+    return "Version 1 work in progress!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
